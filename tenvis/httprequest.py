fd90653c6907af88b754416b358df35416ea6a8d
ysuga <ysuga@ysuga.net>
2014-03-26 11:48:36 +0900

    update for http request

--- tenvis/httprequest.py
+++ tenvis/httprequest.py
@@ -9,7 +9,7 @@ class Request():
         self.__webservie = httplib.HTTP(host)
 
 
-def request(host, url, user, password, message=''):
+def request(host, url, username, password, message=''):
     url = "/get_status.cgi"
     # base64 encode the username and password
     auth = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
@@ -31,3 +31,28 @@ def request(host, url, user, password, message=''):
     print "Headers: ", header
     res = webservice.getfile().read()
     print 'Content: ', res
+
+    vars = res.split(';')
+    dict = {}
+    for var in vars:
+        print var.split()
+        key, val = var.split()[1].split('=')
+        dict[key] = val
+    print dict
+    return dict
+    """
+var id='00656E527CC2';
+var sys_ver='Ver 1.8';
+var app_ver='Ver 1.7.22';
+var alias='IPCamera';
+var now=1395800231;
+var tz=-28800;
+var alarm_status=0;
+var ddns_status=200;
+var ddns_host='mytenvis.org';
+var oray_type=0;
+var upnp_status=4;
+var p2p_status=0;
+var p2p_local_port=25414;
+var msn_status=0;
+"""
